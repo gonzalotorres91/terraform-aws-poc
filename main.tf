dac8694ead62be4216ae32892d947a639ea66b2f
gonzalotorres91 <nikitotorres91@gmail.com>
2025-06-10 00:56:44 +0000

    Agregar configuración terraform y workflow para Terraform Cloud

--- main.tf
+++ main.tf
@@ -1,1 +1,25 @@
-terraform {}
+terraform {
+  required_version = ">= 1.3.0"
+
+  cloud {
+    organization = "devops-poc-org1"
+
+    workspaces {
+      name = "aws-poc"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "poc_bucket" {
+  bucket        = "terraform-poc-${random_id.bucket_id.hex}"
+  force_destroy = true
+}
+
