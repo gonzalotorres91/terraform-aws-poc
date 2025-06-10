ce2d84bb156e6a2e05d9d5f4b9e44595d253e68c
gonzalotorres91 <nikitotorres91@gmail.com>
2025-06-10 03:29:40 +0000

    Corregido main.tf y workflow para Terraform

--- main.tf
+++ main.tf
@@ -1,22 +1,20 @@
-
-
+terraform {
   required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 provider "aws" {
-  region = "us-east-1"  # Cambia a tu región si es necesario
-}
-
-resource "random_id" "bucket_id" {
-  byte_length = 4
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "poc_bucket" {
-  bucket        = "mi-bucket-poc-${random_id.bucket_id.hex}"
+  bucket        = "mi-bucket-terraform-12345"
   force_destroy = true
-
-  tags = {
-    Name        = "poc-bucket"
-    Environment = "Dev"
-  }
 }
+
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,1 @@
-output "bucket_name" {
-  value = aws_s3_bucket.poc_bucket.bucket
-}
+
